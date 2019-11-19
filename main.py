21d6e4d0094dddb1c16226c47afb04358a6a9f6d
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-19 17:29:28 +0700

    first test

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+import numpy as np
+
+
 def t1_file_stat(filename):
     # your solution
     return {
-        "mean": None,
-        "max": None,
-        "min": None,
-        "std_dev": None,
-        "5th_central_moment": None, 
+        "mean": 0.0,
+        "max": 0,
+        "min": 0,
+        "std_dev": 0.0,
+        "5th_central_moment": 0.0,
     }
 
 def t2_sort_int(array):
     pass
 
-def f3_sort_complex(complex_array):
+def t3_sort_complex(complex_array):
     pass
 
-def f4_sort_string_len(string_array):
+def t4_sort_string_len(string_array):
     pass
 
-def f5_sort_string(string_tuple):
+def t5_sort_string(string_tuple):
     pass
--- main_test.py
+++ main_test.py
@@ -1,2 +1,33 @@
-def test_main():
-    pass
+import os
+import random
+import numpy as np
+
+import main
+
+
+def test_t1_file_stat():
+    rng = random.Random(0xDEADBEEF)
+
+    for i, n in enumerate([2, 10, 100, 1000, 1000000]):
+        path = "test_/{}.txt".format(i)
+        data = np.array([int(rng.gauss(0.0, 1.0)) for _ in range(n)])
+        text = "\n".join([str(x) for x in data]) + "\n"
+        try:
+            with open(path, "w") as f:
+                f.write(text)
+            r = main.t1_file_stat(path)
+            print(r)
+
+            mean = data.mean()
+            cm5 = np.mean((data - mean)**5)
+            eps = 1e-8
+            assert abs(r["mean"] - mean) < eps
+            assert abs(r["max"] - data.max()) < eps
+            assert abs(r["min"] - data.min()) < eps
+            assert abs(r["std_dev"] - data.std()) < eps
+            assert abs(r["5th_central_moment"] - cm5) < eps
+        except:
+            os.remove(path)
+            raise
+        else:
+            os.remove(path)
