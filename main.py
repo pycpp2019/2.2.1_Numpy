a00763f361c9e346b0b5cb98065dd19ea536fe71
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-19 16:44:32 +0700

    task description

---
+++ main.py
@@ -0,0 +1,21 @@
+def t1_file_stat(filename):
+    # your solution
+    return {
+        "mean": None,
+        "max": None,
+        "min": None,
+        "std_dev": None,
+        "5th_central_moment": None, 
+    }
+
+def t2_sort_int(array):
+    pass
+
+def f3_sort_complex(complex_array):
+    pass
+
+def f4_sort_string_len(string_array):
+    pass
+
+def f5_sort_string(string_tuple):
+    pass
