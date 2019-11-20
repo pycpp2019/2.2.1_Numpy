d73c0821a9c623420caee68a5c128f48eabb7489
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-20 16:39:56 +0700

    reference solution

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import numpy as np
 
 
 def t1_file_stat(filename):
-    # your solution
+    with open(filename, "r") as f:
+        data = np.array([int(l) for l in [l.strip() for l in f] if len(l) > 0])
+    mean = data.mean()
+    cm5 = np.mean((data - mean)**5)
     return {
-        "mean": 0.0,
-        "max": 0,
-        "min": 0,
-        "std_dev": 0.0,
-        "5th_central_moment": 0.0,
+        "mean": mean,
+        "max": data.max(),
+        "min": data.min(),
+        "std_dev": data.std(),
+        "5th_central_moment": cm5,
     }
 
 def t2_sort_int(array):
-    pass
+    return np.sort(array)
 
 def t3_sort_complex(complex_array):
-    pass
+    abssort = complex_array[np.argsort(np.abs(complex_array))]
+    rresort = np.flip(np.sort(complex_array))
+    return (abssort, rresort)
 
 def t4_sort_string_len(string_array):
-    pass
+    #np.random.shuffle(string_array)
+    return string_array[np.argsort(np.vectorize(len)(string_array))]
 
-def t5_sort_string(string_tuple):
-    pass
+def t5_sort_string_tuple(string_tuple):
+    return tuple(sorted(string_tuple))
