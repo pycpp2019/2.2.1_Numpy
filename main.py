825581d8939de99296346ad3f6242fef7afe6d46
AlexBorodulina <55827253+AlexBorodulina@users.noreply.github.com>
2019-12-01 15:12:45 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import numpy as np
-
-
 def t1_file_stat(filename):
-    # your solution
-    return {
-        "mean": 0.0,
-        "max": 0,
-        "min": 0,
-        "std_dev": 0.0,
-        "5th_central_moment": 0.0,
-    }
-
+	values=np.genfromtxt(filename, delimiter="\n")
+	ans={'mean':0,'max':0,'min':0,'std_dev':0,'5th_central_moment':0}
+	ans['mean']=np.mean(values)
+	ans['max']=np.max(values)
+	ans['min']=np.min(values)
+	m=np.mean(values)
+	s=0
+	for i in values:
+		s+=(i-m)**2
+	ans['std_dev']=(s/len(values))**(1/2)
+	s=0
+	u=dict.fromkeys(np.unique(values),0)
+	for i in values:
+		u[i]+=1
+	for i in values:
+		s+=(i**5)*u[i]/len(values)
+	ans['5th_central_moment']=s
+	return ans
+def sortByLength(inputStr):
+        return len(inputStr)
+#print(t1_file_stat("a.txt"))
 def t2_sort_int(array):
-    pass
+	return np.sort(array)
 
 def t3_sort_complex(complex_array):
-    pass
+	return np.sort(array)
 
 def t4_sort_string_len(string_array):
-    pass
+	sortList=list(string_array)
+	newList=sorted(sortList, key=sortByLength)
+	return np.array(newList)
 
 def t5_sort_string_tuple(string_tuple):
-    pass
+	string_tuple1=np.array(string_tuple,dtype=string)
+	return np.sort(string_tuple1)
+
+#arr=np.array(['j','jfkkpppppp','abc'])
+#print(t4_sort_string_len(arr))
