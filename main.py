78008b0873b08a65eb23cdf9b72701306d91d2dd
Dmitriev Maxim <example.email@example>
2019-12-06 00:19:09 +0700

    numpy and scipy

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import numpy as np
+import scipy.stats as sp
 
 
 def t1_file_stat(filename):
-    # your solution
+    data = np.genfromtxt(filename, delimiter='\n', dtype=np.float)
     return {
-        "mean": 0.0,
-        "max": 0,
-        "min": 0,
-        "std_dev": 0.0,
-        "5th_central_moment": 0.0,
+        "mean": np.mean(data),
+        "max": np.amax(data),
+        "min": np.amin(data),
+        "std_dev": np.std(data),
+        "5th_central_moment": sp.moment(data,5),
     }
 
 def t2_sort_int(array):
-    pass
+    return np.sort(array)
 
 def t3_sort_complex(complex_array):
-    pass
-
+    return (complex_array[np.argsort([abs(num) for num in complex_array])],
+            np.sort_complex(complex_array)[::-1])
+            
 def t4_sort_string_len(string_array):
-    pass
-
+    return string_array[np.argsort(np.array([len(s) for s in string_array]))]
+   
 def t5_sort_string_tuple(string_tuple):
-    pass
+    return tuple(np.sort(np.array(string_tuple)))
