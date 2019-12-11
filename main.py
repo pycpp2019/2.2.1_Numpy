b4a8af8958112381462871f3cf55e29e3502fe57
well120 <n.ushakova1@g.nsu.ru>
2019-12-11 13:57:39 +0700

    Added3

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import numpy as nps
+import numpy as np
 import scipy
 import re
 def t1_file_stat(filename):
-    data = np.loadtxt(filename, delimiter = '\n', dtype = np.int, usecols = len(re.findall(r"[\n']+", filename))) 
-    if data.size <= 2:
+    data_ = np.loadtxt(filename, delimiter = '\n', dtype = np.int, usecols = len(re.findall(r"[\n']+", filename))) 
+    if data_.size <= 2:
         return "Try again"
     else:
         return {                
-        "mean": data.mean(),
-        "max": data.max(),
-        "min": data.min(),
-        "std_dev": data.std(),
-        "5th_central_moment": scipy.stats.moment(data, moment = 5, nan_policy='propagate'),
+        "mean": data_.mean(),
+        "max": data_.max(),
+        "min": data_.min(),
+        "std_dev": data_.std(),
+        "5th_central_moment": scipy.stats.moment(data_, moment = 5, nan_policy='propagate'),
         }
 def t2_sort_int(array):
     return sorted(array)
