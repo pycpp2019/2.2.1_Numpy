13b1d72435e44d7e6a34adc66d6ae6f3d3656339
well120 <n.ushakova1@g.nsu.ru>
2019-12-11 12:38:15 +0700

    Added

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import numpy as np
-
-
+import scipy
+import re
 def t1_file_stat(filename):
-    # your solution
-    return {
-        "mean": 0.0,
-        "max": 0,
-        "min": 0,
-        "std_dev": 0.0,
-        "5th_central_moment": 0.0,
-    }
-
+    data = np.loadtxt(filename, delimiter = '\n', dtype = np.int, usecols = len(re.findall(r"[\n']+", filename))) 
+    if data.size <= 2:
+        return "Try again"
+    else:
+        return {                
+        "mean": data.mean(),
+        "max": data.max(),
+        "min": data.min(),
+        "std_dev": data.std(),
+        "5th_central_moment": scipy.stats.moment(data, moment = 5, nan_policy='propagate'),
+        }
 def t2_sort_int(array):
-    pass
-
+    return sorted(array)
 def t3_sort_complex(complex_array):
-    pass
-
+    a = []
+    b = []
+    for i in complex_array:
+        a.append(abs(i))
+    for j in complex_array:
+        b.append(j.real)
+    return (sorted(a), sorted(b)[::-1])
 def t4_sort_string_len(string_array):
-    pass
-
+    a = []
+    for i in string_array:
+        a.append(len(i))
+    return sorted(a)
 def t5_sort_string_tuple(string_tuple):
-    pass
+    return sorted(sorted(string_tuple), key=str.upper)
