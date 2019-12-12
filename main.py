b8b7d213a8584d91bae32b19902abc0001ffb26e
well120 <n.ushakova1@g.nsu.ru>
2019-12-12 14:36:52 +0700

    Finally added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
-import scipy
+import scipy.stats
 import re
 def t1_file_stat(filename):
     data_ = np.loadtxt(filename, delimiter = '\n', dtype = np.int, usecols = len(re.findall(r"[\n']+", filename))) 
-    if data_.size <= 2:
+    if data_.size < 2:
         return "Try again"
     else:
         return {                
@@ -16,26 +16,8 @@ def t1_file_stat(filename):
 def t2_sort_int(array):
     return sorted(array)
 def t3_sort_complex(complex_array):
-    a = []
-    b = []
-    if len(complex_array) == 0:
-        return "Try again"
-    else:
-        for i in complex_array:
-            a.append(abs(i))
-        for j in complex_array:
-            b.append(j.real)
-        return (sorted(a), sorted(b)[::-1])
+    return (np.array(sorted(complex_array, key = abs)), np.array(sorted(complex_array, key = lambda x: x.real)[::-1]))
 def t4_sort_string_len(string_array):
-    a = []
-    if len(string_array) == 1 or len(string_array) == 0:
-        return "Try again"
-    else:
-        for i in string_array:
-            a.append(len(i))
-        return sorted(a)
+    return np.array(sorted(string_array, key = len))
 def t5_sort_string_tuple(string_tuple):
-    if len(string_tuple) == 1 or len(string_tuple) == 0:
-        return "Try again"
-    else:
-        return sorted(sorted(string_tuple), key=str.upper)
+    return tuple(sorted(string_tuple))
