ecc00527fa7381b3f39bc43a5cfb8cb3c24f5d7b
KarinaSaraeava <karinasarava@gmail.com>
2019-12-12 13:34:58 +0700

    solution

--- main.py
+++ main.py
@@ -31,9 +31,10 @@ def t3_sort_complex(complex_array):
     if len(complex_array) == 0:
         return (np.array([]),np.array([]))
     tup=np.sort_complex(complex_array)
+    
     module_array = np.array([abs(i) for i in complex_array])
     x = module_array.argsort()
-    return (tup, complex_array[x])
+    return (complex_array[x], np.flip(tup,axis=0))
 
 
 def t4_sort_string_len(string_array):
